8609cbd339a8429739da9a5245924be3705c0714
Amat Martínez Vilà <amatmv@gmail.com>
2017-06-06 16:54:00 +0200

    Afegir codi final
    Eso

--- Lloc.java
+++ Lloc.java
@@ -1,23 +1,27 @@
-import java.time.LocalTime;
-import java.util.List;
 import java.util.TimeZone;
 
 /**
  * @class Lloc
- * @brief 
- * @details 
- *
+ * @brief És un lloc amb la seva informació bàsica.
+ * @details Sabem el nom del lloc, les coordenades i la zonaHorària.
  * @author Roger Generoso Masós
  * @version 2017.4.5
  */
 
 public class Lloc{
-    String nom;
-    Float latitud;
-    Float longitud;
-    TimeZone zonaHoraria;
-    List<Estacio> hubs;
+    String nom; ///< És el nom del lloc.
+    Float latitud; ///< És la latitud del lloc.
+    Float longitud; ///< És la longitud del lloc.
+    TimeZone zonaHoraria; ///< És la zona horària del lloc.
 
+    /**
+     * @pre cert
+     * @post Crea un lloc.
+     * @param nomA És el nom del lloc.
+     * @param lat És la latitud del lloc.
+     * @param longit És la longitud del lloc.
+     * @param zonHoraria És la zona horària del lloc.
+     */
     Lloc(String nomA, Float lat, Float longit, TimeZone zonHoraria){
         nom = nomA;
         latitud = lat;
@@ -25,10 +29,40 @@ public class Lloc{
         zonaHoraria = zonHoraria;
     }
 
-    public void afegirPuntInteres(PuntInteres pi){
-
+    @Override
+    public String toString() {
+        return nom;
     }
 
-    public void afegirEstacio(String nomMitjaTransp, LocalTime duradaTrajecte, Float preu) {
+    /**
+     * @pre cert
+     * @return retorna el nom.
+     */
+    public String obtNom() {
+        return nom;
+    }
+
+    /**
+     * @pre cert
+     * @return retorna la longitud del lloc.
+     */
+    public float getLon() {
+        return longitud;
+    }
+
+    /**
+     * @pre cert
+     * @return retorna la zona horària del lloc.
+     */
+    public float getLat() {
+        return latitud;
+    }
+
+    /**
+     * @pre Cert
+     * @return Obtenim la zonaHoraria del lloc.
+     */
+    public TimeZone obtZonHoraria() {
+        return zonaHoraria;
     }
 }
